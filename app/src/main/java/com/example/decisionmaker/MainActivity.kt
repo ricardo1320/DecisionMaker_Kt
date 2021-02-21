14b14b9bdd4f7287121cf9a941c1f885c8c4dc1d
Ricardo Cuevas <ricardo.cuevas3105@gmail.com>
2021-02-21 14:03:55 +0100

    Spin button created

--- app/src/main/java/com/example/decisionmaker/MainActivity.kt
+++ app/src/main/java/com/example/decisionmaker/MainActivity.kt
@@ -5,31 +5,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.example.decisionmaker.views.Roulette
+import android.view.View
 import kotlin.random.Random
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    var myRoulette:Roulette? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myRoulette = findViewById(R.id.roulette2)
-
         //Runnable for animating the roulette
         val rouletteRotThread = Runnable {
             kotlin.run {
-                val animator:ObjectAnimator = ObjectAnimator.ofFloat(myRoulette, "rotation", Random.nextFloat()*720.0f + 720.0f)
+                //val animator:ObjectAnimator = ObjectAnimator.ofFloat(roulette, "rotation", Random.nextFloat()*720.0f + 720.0f)
+                val animator: ObjectAnimator = ObjectAnimator.ofFloat(roulette, "rotation", Random.nextFloat() * 720.0f + 720.0f)
                 animator.setDuration(2000)
                 animator.start()
             }
         }
 
+
         //Post delay runnable for 1500 ms
-        val handler = Handler(Looper.myLooper()!!)
-        handler.postDelayed(rouletteRotThread, 1500)
+        //val handler = Handler(Looper.myLooper()!!)
+        //handler.postDelayed(rouletteRotThread, 1500)
+
+        //Click listener for button_spin (spin the roulette)
+        val listener = View.OnClickListener { view ->
+            when(view.id){
+                R.id.button_spin -> {
+                    rouletteRotThread.run()
+                }
+            }
+        }
+
+        button_spin.setOnClickListener(listener)
 
     }
+
 }
